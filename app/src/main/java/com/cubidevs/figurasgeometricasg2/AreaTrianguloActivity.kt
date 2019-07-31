8913199fb2c7f250c9c67f37770fa59ce25659c0
jhurtado <hurtado0415@gmail.com>
2019-07-31 16:22:03 -0500

    Layout Area de Triangulo G2

---
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/AreaTrianguloActivity.kt
@@ -0,0 +1,12 @@
+package com.cubidevs.figurasgeometricasg2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AreaTrianguloActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_triangulo)
+    }
+}
