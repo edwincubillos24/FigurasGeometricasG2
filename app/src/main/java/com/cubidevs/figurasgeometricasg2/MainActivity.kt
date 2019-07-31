04c30771524294db241fc19f13f43fddedff2916
Emmanuel Gomez Ospina <emmanuelgomezospina@gmail.com>
2019-07-31 16:11:29 -0500

    mainActivity.xml: correction

--- app/src/main/java/com/cubidevs/figurasgeometricasg2/MainActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.cubidevs.figurasgeometricasg2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var intentsquarePerimeterActivity=Intent(this,squarePerimeterActivity::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 R.id.bnCuadrado -> {
-
+                    startActivity(intentsquarePerimeterActivity)
                 }
                 R.id.bnRectangulo -> {
 
---
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/squarePerimeterActivity.kt
@@ -0,0 +1,12 @@
+package com.cubidevs.figurasgeometricasg2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class squarePerimeterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_square_perimeter)
+    }
+}
