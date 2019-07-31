5dd6de09d45f3d99fddefb08f70c682b382db090
jsebguerrero <jsebastian.guerrero@gmail.com>
2019-07-31 15:53:30 -0500

    square area feature added

---
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/SquareAreaActivity.kt
@@ -0,0 +1,12 @@
+package com.cubidevs.figurasgeometricasg2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SquareAreaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_square_area)
+    }
+}
