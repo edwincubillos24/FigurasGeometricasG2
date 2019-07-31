81eb18497d2731da5dfe72080337aa436f463dc2
edwinacubillos <edwinacubillos@gmail.com>
2019-07-31 15:51:20 -0500

    Version inicial

---
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/SelAreaPerActivity.kt
@@ -0,0 +1,12 @@
+package com.cubidevs.figurasgeometricasg2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelAreaPerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sel_area_per)
+    }
+}
