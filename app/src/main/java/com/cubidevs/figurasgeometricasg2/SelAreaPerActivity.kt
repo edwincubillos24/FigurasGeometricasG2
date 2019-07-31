695e7207f7f4cc08acb10998719ac2123de6714b
edwinacubillos <edwinacubillos@gmail.com>
2019-07-31 16:33:14 -0500

    Done

--- app/src/main/java/com/cubidevs/figurasgeometricasg2/SelAreaPerActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/SelAreaPerActivity.kt
@@ -1,7 +1,9 @@
 package com.cubidevs.figurasgeometricasg2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SelAreaPerActivity : AppCompatActivity() {
 
@@ -9,4 +11,62 @@ class SelAreaPerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sel_area_per)
     }
+
+    fun onButtonClicked(view: View) {
+        var figurarecibida = intent.extras
+        var data = figurarecibida.getInt("figura")
+        if (view is Button) {
+            if (view.getId()==R.id.bnArea){
+                when (data){
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    2 -> {
+
+                    }
+                    3 -> {
+
+                    }
+                    4 -> {
+
+                    }
+                    5 -> {
+
+                    }
+                    6 -> {
+
+                    }
+                }
+            }
+            if (view.getId()==R.id.bnPerimetro) {
+                when (data) {
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    2 -> {
+
+                    }
+                    3 -> {
+
+                    }
+                    4 -> {
+
+                    }
+                    5 -> {
+
+                    }
+                    6 -> {
+
+                    }
+                }
+            }
+        }
+    }
 }
+
