7dc2aad05f784ac37a7cf0d662c285bb55a345c7
Emmanuel Gomez Ospina <emmanuelgomezospina@gmail.com>
2019-08-05 13:33:14 -0500

    LinearLayout

--- app/src/main/java/com/cubidevs/figurasgeometricasg2/squarePerimeterActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/squarePerimeterActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class squarePerimeterActivity : AppCompatActivity() {
+    var reseivedData=intent.extras
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_square_perimeter)
     }
+
+
 }
