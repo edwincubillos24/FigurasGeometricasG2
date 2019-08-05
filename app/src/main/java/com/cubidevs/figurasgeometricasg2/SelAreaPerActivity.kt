087b18dd53883b51ba706da53256864ea2901449
Emmanuel Gomez Ospina <emmanuelgomezospina@gmail.com>
2019-08-05 16:50:45 -0500

    Merge branch 'develop' of https://github.com/udeag2/FigurasGeometricasG2 into feature_selSquarePerimeter
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/cubidevs/figurasgeometricasg2/MainActivity.kt
    #	app/src/main/res/values/strings.xml
    
    operative "things" finished.

--- app/src/main/java/com/cubidevs/figurasgeometricasg2/SelAreaPerActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/SelAreaPerActivity.kt
@@ -1,12 +1,13 @@
 package com.cubidevs.figurasgeometricasg2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SelAreaPerActivity : AppCompatActivity() {
-
+    private var intentSquarePerimeter = Intent(this,squarePerimeterActivity::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sel_area_per)
@@ -14,15 +15,14 @@ class SelAreaPerActivity : AppCompatActivity() {
 
     fun onButtonClicked(view: View) {
         var figurarecibida = intent.extras
-        var data = figurarecibida.getInt("figura")
+        var data = figurarecibida!!.getInt("figura")
         if (view is Button) {
             if (view.getId()==R.id.bnArea){
                 when (data){
                     0 -> {
-
                     }
                     1 -> {
-
+                        startActivity(intentSquarePerimeter)
                     }
                     2 -> {
 
--- app/src/main/java/com/cubidevs/figurasgeometricasg2/squarePerimeterActivity.kt
+++ app/src/main/java/com/cubidevs/figurasgeometricasg2/squarePerimeterActivity.kt
@@ -2,14 +2,26 @@ package com.cubidevs.figurasgeometricasg2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_square_perimeter.*
 
 class squarePerimeterActivity : AppCompatActivity() {
-    var reseivedData=intent.extras
+    //var reseivedData=intent.extras
+    private var r=0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_square_perimeter)
     }
 
+    fun solveSquarePerimeter(view: View){
+        if (view is Button){
+            var a=et_square_side_ego2509.text.toString().toFloat()
+            r=4*a
+            tx_outp_square_perim_ego2509.text=r.toString()
+        }
+    }
 
 }
